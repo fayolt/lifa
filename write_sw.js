cb9ca7c2d37a7a899d9bf7448fa46ce55ec7b244
Hippolyte-Fayol Toulassi <fayolt@live.fr>
2020-11-18 00:49:29 +0000

    fix vulnerabilities in dependencies

--- write_sw.js
+++ write_sw.js
@@ -1,11 +1,9 @@
 const sw = require( "sw-precache" );
 
-{
-    const rootDir = "./";
-    const distDir = "./_site";
-  
-    return sw.write( `${rootDir}/sw.js`, {
-      staticFileGlobs: [ distDir + "/**/*.{js,html,css,png,jpg,svg}" ],
-      stripPrefix: distDir
-    } );
-  }
+const rootDir = "./";
+const distDir = "./_site";
+
+sw.write( `${rootDir}/sw.js`, {
+  staticFileGlobs: [ distDir + "/**/*.{js,html,css,png,jpg,svg}" ],
+  stripPrefix: distDir
+} )
